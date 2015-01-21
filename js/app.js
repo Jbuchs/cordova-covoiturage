c9524a3d5429acb43251aab01ed48e709156e2e7
Joel <joel.buchs@cpnv.ch>
2015-01-21 10:56:03 +0100

    création de trajet

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ app.config(function($routeProvider){
    $routeProvider
        .when('/home', {templateUrl: 'pages/home.html'})
        .when('/tes_trajets', {templateUrl: 'pages/tes_trajets.html'})
-       .when('/conducteur', {templateUrl: 'pages/conducteur.html'})
+       .when('/conducteur', {templateUrl: 'pages/creation_trajet.html'})
        .when('/passager', {templateUrl: 'pages/passager.html'})
        .when('/menu', {templateUrl: 'pages/menu.html'})
        .when('/donnees_personnelles', {templateUrl: 'pages/donnees_personnelles.html'})
