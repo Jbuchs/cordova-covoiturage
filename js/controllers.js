cd964b48afacbfaf7d39653b0d8b547b3c184c95
Joel <joel.buchs@cpnv.ch>
2015-01-29 11:20:12 +0100

    controllers ok

--- js/controllers.js
+++
@@ -1,8 +0,0 @@
-var countryControllers = angular.module('dataControllers',[]);
-
-countryControllers.controller('CountryDetailCtrl', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
-        $http.get('datas/' + $routeParams.countryId + '.json').success(function(data) {
-            $scope.country = data;
-        });
-    }]);
